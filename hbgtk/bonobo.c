982fbbb8953491aa8b7b60a0af21cad3d88b97bc
xthefull <xthefull@dd9ddd24-974d-4448-9c2b-4c2dd08e6bec>
2008-11-08 17:40:34 +0000

    Sistema de componentes BONOBO
    git-svn-id: http://www.gtxbase.org/tgtkdevel/trunk@160 dd9ddd24-974d-4448-9c2b-4c2dd08e6bec

---
+++ hbgtk/bonobo.c
@@ -0,0 +1,39 @@
+#include "hbapi.h"
+#include "hbapiitm.h"
+
+#ifdef _HAVEBONOBO_
+
+#include <libbonobo.h>
+#include <libbonoboui.h>
+
+HB_FUNC( BONOBO_INIT )
+{
+    CORBA_ORB orb;
+    
+    orb = bonobo_activation_init( 0, NULL );
+    if (bonobo_init( 0, NULL ) == FALSE) {
+        g_error("No se pudo inicializar Bonobo\n");
+     }
+    bonobo_activate();
+    g_warning( "Activate BONOBO" );
+}
+
+HB_FUNC( BONOBO_WIDGET )
+{
+  GtkWidget * ventana = GTK_WIDGET( hb_parnl( 1 ) ); 
+   // const gchar* interfaces[] = { "IDL:Bonobo/Control:1.0", NULL };
+  //  gchar* seleccionado = bonobo_selector_select_id("Selecciona control", interfaces);
+  // if (seleccionado) {
+        g_warning( "Seleccionado...\n" );
+        GtkWidget* control = bonobo_widget_new_control( hb_parc( 2 ), NULL);
+        gtk_container_add(GTK_CONTAINER(ventana), control);
+        gtk_widget_show( control );
+    //  }
+
+}
+
+HB_FUNC( BONOBO_MAIN_QUIT )
+{
+  bonobo_main_quit();
+}
+#endif
