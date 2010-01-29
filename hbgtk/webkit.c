f26a14ccd0d3aab6ed96d06fe7f0173c1015a397
xthefull <xthefull@dd9ddd24-974d-4448-9c2b-4c2dd08e6bec>
2010-01-29 21:53:22 +0000

    Soporte para WebKit
    git-svn-id: http://www.gtxbase.org/tgtkdevel/trunk@213 dd9ddd24-974d-4448-9c2b-4c2dd08e6bec

---
+++ hbgtk/webkit.c
@@ -0,0 +1,36 @@
+#include "hbapi.h"
+#include "hbapiitm.h"
+
+#ifdef _WEBKIT_
+#include <gtk/gtk.h>
+#include <webkit/webkit.h>
+
+HB_FUNC( WEBKIT_WEB_VIEW )
+{
+ hb_retnl( hb_parnl( 1 ) );
+}
+
+HB_FUNC( WEBKIT_WEB_VIEW_NEW )
+{
+   GtkWidget * webkit= webkit_web_view_new();
+   hb_retnl( (glong) webkit );
+}
+
+HB_FUNC( WEBKIT_WEB_VIEW_OPEN )
+{
+
+   webkit_web_view_open( WEBKIT_WEB_VIEW ( hb_parnl( 1 ) ), hb_parc( 2 ) );
+}
+
+/*
+Para la version 1.1
+void                webkit_web_view_load_uri            (WebKitWebView *web_view,
+                                                         const gchar *uri);
+
+HB_FUNC( WEBKIT_WEB_VIEW_LOAD_URI )
+{
+
+   webkit_web_view_load_uri( WEBKIT_WEB_VIEW ( hb_parnl( 1 ) ), hb_parc( 2 ) );
+}
+*/
+#endif
