1265043754ec1a479d3576741e64744dbe468b4c
riztan <riztan@dd9ddd24-974d-4448-9c2b-4c2dd08e6bec>
2011-05-13 02:28:35 +0000

    + pkg_install/miscelan_bin/7z.dll     Necesario para descomprimir
      * config/unzip.mk
        Se cambia ruta para descomprimir en GTK_PATH
        Se crea gtkrc con tema de Windows
    
    git-svn-id: http://www.gtxbase.org/tgtkdevel/trunk@452 dd9ddd24-974d-4448-9c2b-4c2dd08e6bec

--- config/unzip.mk
+++ config/unzip.mk
@@ -29,8 +29,8 @@ $(info Ejecutando config/unzip.mk)
      $(info Debera realizar la extraccion manualmente y colocar los archivos )
      $(info en $(TGTK_BIN). )
      $(info --------------- )
+     export TGTK_UNZIP :=no
    endif
-   export TGTK_UNZIP :=no
  endif
 
 
@@ -45,76 +45,79 @@ ifneq ($(notdir $(wildcard $(DIR_DOWN)/*)),)
 
   ifeq ($(TGTK_UNZIP),yes)
     $(info Descomprimiendo gLib for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GLIB_BIN_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GLIB_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GLIB_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GLIB_DEV_FILE).zip )
 
     $(info Descomprimiendo Pkg-Config for Windows ... )
     #$(info $(EXECUTE)$(DIR_DOWN) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).tar.gz)
     #$(shell $(EXECUTE)$(DIR_DOWN) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).tar.gz )
-    #$(info $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).zip)
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).zip )
+    #$(info $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).zip)
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PKGCONFIG_BIN_FILE).zip )
 
     $(info Descomprimiendo GDK-PixBuf for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GDK_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GDK_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GDK_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GDK_BIN_FILE).zip )
 
     $(info Descomprimiendo Pango for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PANGO_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PANGO_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PANGO_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PANGO_BIN_FILE).zip )
 
     $(info Descomprimiendo FreeType for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(FTYPE_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(FTYPE_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(FTYPE_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(FTYPE_BIN_FILE).zip )
 
     $(info Descomprimiendo FontConfig for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(FONT_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(FONT_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(FONT_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(FONT_BIN_FILE).zip )
 
     $(info Descomprimiendo LibPNG for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PNG_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(PNG_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PNG_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(PNG_BIN_FILE).zip )
 
     $(info Descomprimiendo Cairo for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(CAIRO_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(CAIRO_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(CAIRO_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(CAIRO_BIN_FILE).zip )
 
     $(info Descomprimiendo ATK for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(ATK_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(ATK_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(ATK_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(ATK_BIN_FILE).zip )
 
     $(info Descomprimiendo GTK+ Runtime for Windows ... )
-#    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GTK22_BIN_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GTK+_FILE).zip )
+#    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GTK22_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GTK+_FILE).zip )
 
     $(info Descomprimiendo GTK+ Development for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GTK+_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GTK+_DEV_FILE).zip )
 
     $(info Descomprimiendo LibXML-2 for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(XML_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(XML_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(XML_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(XML_BIN_FILE).zip )
 
     $(info Descomprimiendo LibXML2-2 for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(XML2_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(XML2_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(XML2_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(XML2_BIN_FILE).zip )
 
     $(info Descomprimiendo Glade for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(LIBGLADE_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(LIBGLADE_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(LIBGLADE_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(LIBGLADE_BIN_FILE).zip )
     #$(shell $(DIR_DOWN)\$(GLADE_BIN_FILE).exe )
 
     $(info Descomprimiendo ZLIB for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(ZLIB_DEV_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(ZLIB_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(ZLIB_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(ZLIB_BIN_FILE).zip )
 
     $(info Descomprimiendo GetText for Windows ... )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GETTEXT_BIN_FILE).zip )
-    $(shell $(EXECUTE)$(TGTK_BIN) $(DIR_DOWN)\$(GETTEXT_DEV_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GETTEXT_BIN_FILE).zip )
+    $(shell $(EXECUTE)$(GTK_PATH) $(DIR_DOWN)\$(GETTEXT_DEV_FILE).zip )
 
     $(info Ejecutando Instalador de OpenSSQL for Windows ... )
     $(shell $(DIR_DOWN)\$(OPENSSL_INST_FILE) )
 
     export TGTK_UNZIP :=no
     $(shell echo yes > $(TGTK_DIR)\config\unziped.log)
+
+    #-- Aplicamos tema de Windows.. ;)
+    $(shell echo gtk-theme-name = "MS-Windows" > $(GTK_PATH)\etc\gtk-2.0\gtkrc )
   endif
 
    
