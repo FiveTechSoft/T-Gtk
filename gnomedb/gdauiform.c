0a73e91ab90a4b135777c961a25ba5ba4339a8a3
riztan <riztan@dd9ddd24-974d-4448-9c2b-4c2dd08e6bec>
2012-08-22 08:12:50 +0000

    Continuacion LibGDA.  + gnomedb/gdauiform.c  + gnomedb/gdauirawform.c  + gnomedb/gdauirawgrid.c
    git-svn-id: http://www.gtxbase.org/tgtkdevel/trunk@563 dd9ddd24-974d-4448-9c2b-4c2dd08e6bec

---
+++ gnomedb/gdauiform.c
@@ -0,0 +1,44 @@
+/* $Id: gdauiform.c,v 1 2012-08-22 03:17:11 riztan Exp $*/
+/*
+    LGPL Licence.
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this software; see the file COPYING.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+    Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
+
+    LGPL Licence.
+    (c)2008 Rafael Carmona <rafa.tgtk at gmail.com>
+    (c)2012 Riztan Gutierrez <riztan at t-gtk.org>
+*/
+#include <hbapi.h>
+
+#ifdef _GDA_
+#include <gtk/gtk.h>
+#include <libgda/libgda.h>
+#include <libgda-ui/libgda-ui.h>
+
+
+HB_FUNC( GDAUI_FORM_NEW ) 
+{
+   GtkWidget * form;
+   GdaDataModel * model = hb_parptr( 1 );
+
+   form = gdaui_form_new( model );
+   hb_retptr( form );
+}
+
+
+#endif
+
+//eof
