c2891a5c7142b3b0477c159387541dd2a2c7d742
Riztan <riztan@gmail.com>
2013-08-14 01:38:19 -0430

    /hbgtk/gtkfileselec.c  pendiente terminar modificaciones en GTK_FILE_SELECTION_CONNECT  falta completar los cambios para realizar la conexion de señales al boton OK  he actualizado la rama por seguridad... (mantener cambios) Espero continuar mañana.

--- hbgtk/gtkfileselec.c
+++ hbgtk/gtkfileselec.c
@@ -33,7 +33,7 @@
 #include "hbapiitm.h"
 #include "hbapierr.h"
 
-#if GTK_MAJOR_VERSION > 3
+#if GTK_MAJOR_VERSION < 3
 HB_FUNC( GTK_FILE_SELECTION_NEW )
 {
   GtkWidget * filesel;
@@ -107,40 +107,109 @@ HB_FUNC( GTK_FILE_CHOOSER_DIALOG_NEW )
 #endif
 
 #if GTK_MAJOR_VERSION < 3
-HB_FUNC( GTK_FILE_SELECTION_HIDE_FILEOP_BUTTONS )
+HB_FUNC( GTK_FILE_SELECTION_GET_FILENAME )
 {
   GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
+  const gchar * sel_filename = gtk_file_selection_get_filename( filesel );
+#else
+HB_FUNC( GTK_FILE_SELECTION_GET_FILENAME )
+{
+  GtkWidget * dialog = GTK_WIDGET( hb_parptr( 1 ) );
+  const gchar * sel_filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER( dialog ) );
+#endif
+  hb_retc( ( gchar * ) sel_filename );
+}
+
+
+HB_FUNC( GTK_FILE_SELECTION_HIDE_FILEOP_BUTTONS )
+{
+#if GTK_MAJOR_VERSION < 3
+  GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
   gtk_file_selection_hide_fileop_buttons( filesel );
+#else
+  g_message("gtk_file_selection_hide_fileop_buttons is deprecated!");
+#endif
 }
 
 HB_FUNC( GTK_FILE_SELECTION_SHOW_FILEOP_BUTTONS )
 {
+#if GTK_MAJOR_VERSION < 3
   GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
   gtk_file_selection_show_fileop_buttons( filesel );
+#else
+  g_message("gtk_file_selection_show_fileop_buttons deprecated!");
+#endif
 }
 
+
+#if GTK_MAJOR_VERSION < 3
 HB_FUNC( GTK_FILE_SELECTION_SET_SELECT_MULTIPLE )
 {
   GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
   gboolean select = ( gboolean ) hb_parl( 2 );
   gtk_file_selection_set_select_multiple( filesel, select );
 }
-
-HB_FUNC( GTK_FILE_SELECTION_GET_FILENAME )
+#else
+HB_FUNC( GTK_FILE_SELECTION_SET_SELECT_MULTIPLE )
 {
-  GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
-  const gchar * sel_filename = gtk_file_selection_get_filename( filesel );
-  hb_retc( ( gchar * ) sel_filename );
+  GtkFileChooser * filesel = GTK_FILE_CHOOSER( hb_parptr( 1 ) );
+  gboolean select = ( gboolean ) hb_parl( 2 );
+  gtk_file_chooser_set_select_multiple( filesel, select );
 }
+
+HB_FUNC( GTK_FILE_CHOOSER_SET_SELECT_MULTIPLE )
+{
+  GtkFileChooser * filesel = GTK_FILE_CHOOSER( hb_parptr( 1 ) );
+  gboolean select = ( gboolean ) hb_parl( 2 );
+  gtk_file_chooser_set_select_multiple( filesel, select );
+}
+#endif
 
 // TODO: Atencion , tenemos que tener en cuenta el tema del g_filename_from_utf8
+#if GTK_MAJOR_VERSION < 3
 HB_FUNC( GTK_FILE_SELECTION_SET_FILENAME )
 {
-   GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
-   gchar * file = ( gchar * ) hb_parc( 2 );
-   gtk_file_selection_set_filename( filesel, file );
+  GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
+  gchar * file = ( gchar * ) hb_parc( 2 );
+  gtk_file_selection_set_filename( filesel, file );
+}
+#else
+HB_FUNC( GTK_FILE_CHOOSER_SET_FILENAME )
+{
+  GtkFileChooser * filesel = GTK_FILE_CHOOSER( hb_parptr( 1 ) );
+  const gchar * file = ( gchar * ) hb_parc( 2 );
+  gtk_file_chooser_set_filename( filesel, file );
 }
 
+HB_FUNC( GTK_FILE_SELECTION_SET_FILENAME )
+{
+  g_message("gtk_file_selection_set_filename is deprecated!");
+  GtkFileChooser * filesel = GTK_FILE_CHOOSER( hb_parptr( 1 ) );
+  const gchar * file = ( gchar * ) hb_parc( 2 );
+  gtk_file_chooser_set_filename( filesel, file );
+}
+#endif
+
+
+static void event_filename( GtkWidget *widget, gpointer user_data )
+{
+   PHB_ITEM pUser_Data = ( PHB_ITEM ) user_data;
+   PHB_ITEM pBlock     = hb_arrayGetItemPtr( pUser_Data, 1 );
+   PHB_ITEM pWidget    = hb_itemNew( 0 );
+
+   if( HB_IS_BLOCK( pBlock ) )
+   {
+      hb_itemPutPtr( pWidget, widget );
+      hb_vmEvalBlockV( pBlock, 1, pWidget );
+      hb_itemRelease( pWidget );
+      hb_itemRelease( pUser_Data );
+
+   }
+  
+}
+
+
+#if _GTK2_
 HB_FUNC( GTK_FILE_SELECTION_GET_SELECTIONS )
 {
  /* GTK+ Reference Manual
@@ -173,23 +242,6 @@ HB_FUNC( GTK_FILE_SELECTION_COMPLETE )
    gtk_file_selection_complete( filesel, pattern );
 }
 
-static void event_filename( GtkWidget *widget, gpointer user_data )
-{
-   PHB_ITEM pUser_Data = ( PHB_ITEM ) user_data;
-   PHB_ITEM pBlock     = hb_arrayGetItemPtr( pUser_Data, 1 );
-   PHB_ITEM pWidget    = hb_itemNew( 0 );
-
-   if( HB_IS_BLOCK( pBlock ) )
-   {
-      hb_itemPutPtr( pWidget, widget );
-      hb_vmEvalBlockV( pBlock, 1, pWidget );
-      hb_itemRelease( pWidget );
-      hb_itemRelease( pUser_Data );
-
-   }
-  
-}
-
 HB_FUNC( GTK_FILE_SELECTION_CONNECT )
 {
    GtkFileSelection * filesel = GTK_FILE_SELECTION( hb_parptr( 1 ) );
@@ -216,5 +268,41 @@ HB_FUNC( GTK_FILE_SELECTION_CONNECT )
 
 }
 
+#else
+HB_FUNC( GTK_FILE_SELECTION_CONNECT )
+{
+   GtkFileChooser * filesel = GTK_FILE_CHOOSER( hb_parptr( 1 ) );
+   const char * szsignal = hb_parc( 2 );
+   PHB_ITEM pBlock       = hb_param( 3, HB_IT_BLOCK );
+   PHB_ITEM pUser_Data   = hb_itemArrayNew( 1 );
+   gint response = gtk_dialog_run( GTK_DIALOG ( filesel ) );
+   
+   
+   
+   if( HB_IS_BLOCK( pBlock ) ){
+      hb_itemArrayPut( pUser_Data, 1, pBlock );
+
+//      if( hb_stricmp( szsignal, "ok_button" ) == 0 )
+      if ( response == GTK_RESPONSE_OK || response == GTK_RESPONSE_ACCEPT )
+      {
+         g_message("AQUIIII");
+/*
+         g_signal_connect (GTK_FILE_CHOOSER (filesel)->ok_button,
+                           "clicked",
+                            G_CALLBACK(event_filename),
+                            (gpointer) pUser_Data ); 
+         g_signal_connect_swapped (GTK_FILE_CHOOSER (filesel)->ok_button,
+                           "clicked",
+                            G_CALLBACK(gtk_widget_destroy ),
+                            (gpointer) filesel );
+*/
+      }
+   }
+
+}
+
 #endif
+
+//eof
+
 
